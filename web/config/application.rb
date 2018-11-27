f3589f07c6c96fc21becd50299bafbe92ccbf7a5
sirka-dev <45910564+sirka-dev@users.noreply.github.com>
2018-11-27 16:57:48 +0900

    gem追加

--- web/config/application.rb
+++ web/config/application.rb
@@ -27,7 +27,11 @@ module App
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    # Don't generate system test files.
-    config.generators.system_tests = nil
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.system_tests nil
+      g.template_engine :slim
+    end
   end
 end
