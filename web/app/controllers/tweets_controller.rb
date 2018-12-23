2649f7cfeda95445772ff361400fcd83980fe70a
sirka-dev <45910564+sirka-dev@users.noreply.github.com>
2018-12-23 14:23:46 +0900

    使っていないtweet/timelineを削除

--- web/app/controllers/tweets_controller.rb
+++ web/app/controllers/tweets_controller.rb
@@ -1,26 +1,10 @@
 # frozen_string_literal: true
 
 class TweetsController < ApplicationController
-  before_action :set_client, only: [:index, :timeline]
+  before_action :set_client, only: [:index]
 
   def index
     @tweets = Tweet.all
-  end
-
-  def timeline
-    mecab = Natto::MeCab.new(dicdir: '/usr/local/lib/mecab/dic/mecab-ipadic-neologd')
-
-    timelines = @client.list_timeline('sirka_p', 'アイマス情報源')
-    @wakachi = []
-    timelines.each do |t|
-      text = t.text || t.attrs[:full_text]
-      hash = {}
-      mecab.enum_parse(text).each do |node|
-        hash[node.surface] = node.feature.split(',')[0] unless node.is_eos?
-      end
-      @wakachi.push(hash)
-    end
-    @tweets = timelines
   end
 
   private
--- web/config/routes.rb
+++ web/config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   resources :lives
   root to: 'goiryoku#index'
   get 'tweets/index'
-  get 'tweets/timeline'
   get 'goiryoku/index'
   get 'goiryoku/goiryoku'
 end
